caeac5b58b98e2255959a017e64927085ede955a
Vidisha Attili <76433235+vidisha0205@users.noreply.github.com>
2023-02-16 11:01:54 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello World, this is PES1UG20CS091\n";
-  cout << "Hello Jenkins, this is PES1UG20CS091\n";
-  cout << "I have successfully built and run\n";
+  cout << "Hello World, PES1UG20CS091\n";
+  cout << "Hello Jenkins, PES1UG20CS091\n";
+  cout << "I have successfully built and run.\n";
+  cout <<"hello.cpp has been changed for task 4.\n";
   return 0;
 }
