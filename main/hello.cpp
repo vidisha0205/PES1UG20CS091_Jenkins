1b90770656fffd70639b4cefaa075817e84f3c13
Vidisha Attili <76433235+vidisha0205@users.noreply.github.com>
2023-02-16 10:20:17 +0530

    Create hello.cpp

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello World, this is PES1UG20CS091\n";
+  cout << "Hello Jenkins, this is PES1UG20CS091\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
